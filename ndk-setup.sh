a1e8c4d1dca2ab30ef7f528bada1b469dbed6051
Mr. Happy Nice <themrhappynice@gmail.com>
2024-12-28 08:48:53 -0600

    Update ndk-setup.sh

--- ndk-setup.sh
+++ ndk-setup.sh
@@ -2,6 +2,8 @@
 
 set -e 
 
+echo "Downloading and extracting NDK..."
+
 curl -sSL https://dl.google.com/android/repository/android-ndk-r23b-linux.zip -o /tmp/android-ndk.zip
 
 sudo unzip /tmp/android-ndk.zip -d /usr/local/
