bb7c4dd131ff33282328845123c5a1127bb0ef8b
Mr. Happy Nice <themrhappynice@gmail.com>
2024-12-28 08:39:04 -0600

    Update ndk-setup.sh

--- ndk-setup.sh
+++ ndk-setup.sh
@@ -6,8 +6,4 @@ curl -sSL https://dl.google.com/android/repository/android-ndk-r23b-linux.zip -o
 
 sudo unzip /tmp/android-ndk.zip -d /usr/local/
 
-source exports.sh
 
-echo 'export ANDROID_NDK_HOME=/usr/local/android-ndk-r23b' >> ~/.bashrc
-echo 'export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
